fae8022231a6af715371f421978ac74864e5eeda
Muhammad Hussain <muhammad.hussain.cs8@gmail.com>
2025-02-24 14:22:43 +0500

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const connectDB = async ()=>{
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/user')
+        const conn = await mongoose.connect('mongodb://localhost:27017/mydatabase')
         console.log(`mongo db connected at ${conn.connection.host}` );
     } catch (error) {
         console.log(error);
